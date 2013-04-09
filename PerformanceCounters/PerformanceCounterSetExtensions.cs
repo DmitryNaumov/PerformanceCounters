e4525db1a8a16a3eb274ae20598d5a8bb8e58cdb
Dmitry Naumov <dnaumov@paladyne.com>
2013-04-09 16:40:12 +0400

    Code cleanup Added Clr Jit counters

--- PerformanceCounters/PerformanceCounterSetExtensions.cs
+++ PerformanceCounters/PerformanceCounterSetExtensions.cs
@@ -2,7 +2,6 @@ namespace PerformanceCounters
 {
 	using System;
 	using System.Linq;
-	using System.Linq.Expressions;
 
 	public static class PerformanceCounterSetExtensions
 	{
@@ -16,25 +15,6 @@ namespace PerformanceCounters
 
 			var categoryAttribute = (PerformanceCounterCategoryAttribute)attributes[0];
 			return categoryAttribute.CategoryName;
-		}
-
-		public static string GetCounterName<T, TResult>(this T counterSet, Expression<Func<T, TResult>> selector) where T : IPerformanceCounterSet
-		{
-			var expression = selector.Body as MemberExpression;
-			if (expression == null)
-			{
-				throw new ArgumentException("selector");
-			}
-
-			var attributes = expression.Member.GetCustomAttributes(typeof (PerformanceCounterAttribute), false);
-			if (attributes.Length != 1)
-			{
-				throw new ArgumentException("counterSet");
-			}
-
-			var counterAttribute = (PerformanceCounterAttribute)attributes[0];
-
-			return counterAttribute.CounterName;
 		}
 
 		public static string[] GetCounterNames(this IPerformanceCounterSet counterSet)
